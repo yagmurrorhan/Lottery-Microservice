6da19076932d95986762d48b14848c87dcb13a6a
Yağmur ORHAN <yagmurrorhan@gmail.com>
2022-03-25 23:27:46 +0300

    Adding documents

---
+++ src/main/java/com/example/lottery/LotteryMicroserviceApplication.java
@@ -0,0 +1,19 @@
+package com.example.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCaching
+public class LotteryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryMicroserviceApplication.class, args);
+	}
+
+}
